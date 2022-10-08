194ea040df6d906afcd8ef14382cc2d3b63ef32b
rutthy52rg <rutthy52.rg@gmail.com>
2022-10-08 11:15:23 +0200

    created apiproviders file and added post method

--- ApiProviders.js
+++ ApiProviders.js
@@ -1,30 +1,37 @@
 class ApiProviders {
   baseUrl = "http://localhost:8000";
   endpoints = {
-    signup: "/auth/register"
+    login: "/auth/login",
+    signup: "/auth/register",
+    // announcements: "/api/announcements",
   };
   constructor() {}
 
+  // async get(endpoint) {
+  //   const response = await fetch(`${this.baseUrl}${endpoint}`);
+
+  //   if (!response.ok) {
+  //     throw new Error("No existen resultados");
+  //   }
+
+  //   const data = await response.json();
+
+  //   return data;
+  // }
 
   async post(endpoint, body) {
-    //una vez logado cogemos el token que genera 
-    const token = localStorage.getItem("token");
-    // endpoint post
+    const token = localStorage.getItem("token"); //necesario este método porque en cabecera del cuerpo se especifica la autorización por este método. lin.29
     const response = await fetch(`${this.baseUrl}${endpoint}`, {
       method: "POST",
-      //pasamos a string los datos del formulario para mandarlo a bbdd
       body: JSON.stringify(body),
-      //cabecera para 
       headers: {
         "Content-Type": "application/json",
-        //tipo de autorización
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${token}`, //necesaria autorización por token para método post
       },
     });
 
-    //cogemos y devolvemos la respuesta en json
     const data = await response.json();
-    console.log(data)
+
     return data;
   }
 
