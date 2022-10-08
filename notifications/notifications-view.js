fa63c37b54285d67385025461a75fce3c7e536e0
rutthy52rg <rutthy52.rg@gmail.com>
2022-10-08 23:09:49 +0200

    added modal and sparated design notifications betwen error and status

--- notifications/notifications-view.js
+++ notifications/notifications-view.js
@@ -1,6 +1,42 @@
-export const buildNotication = (message) => {
-  const htmlTemplate = `<p>${message}</p>
-  <button class="notification-button-close">cerrar</button>
+export const notificationStatusTemplateHTML = (message) => {
+  const htmlTemplate = `
+  <div class="alert alert-info alert-dismissible fade show" role="alert">
+      <strong><p>${message}</p></strong> You should check in on some of those fields below.
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>
 `;
+  return htmlTemplate;
+};
+
+export const notificationErrorTemplateHTML = (message) => {
+  const htmlTemplate = `
+  <div class="modal" tabindex="-1">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Modal title</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <p>${message}</p>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary close-button"
+            data-bs-dismiss="modal"
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>`;
+
   return htmlTemplate;
 };
--- notifications/notificationsController.js
+++ notifications/notificationsController.js
@@ -1,27 +1,45 @@
+import {
+  notificationErrorTemplateHTML,
+  notificationStatusTemplateHTML
+} from "./notifications-view.js";
 import { pubSub } from "./PubSub.js";
-import { buildNotication } from "./notifications-view.js";
 
 export class NotificationController {
   constructor(nodeElement) {
     this.notificationElement = nodeElement;
-    this.subscribeToNotification();
+    this.subscribeToNotificationError();
+    this.subscribeToNotificationStatus();
   }
 
   //me suscribo con el método subscribe a los (publish) cambios en pubsub y le meto por parámetro el mensaje de error que le llegará desde los controladores que lo llamen a través de pubsub.publish
-  subscribeToNotification() {
-    pubSub.subscribe(pubSub.TOPICS.NOTIFICATION, (message) => {
-      this.printNotification(message);
+  subscribeToNotificationError() {
+    pubSub.subscribe(pubSub.TOPICS.NOTIFICATION_ERROR, (message) => {
+      this.printNotificationError(message);
+    });
+  }
+  subscribeToNotificationStatus() {
+    pubSub.subscribe(pubSub.TOPICS.NOTIFICATION_STATUS, (message) => {
+      this.printNotificationStatus(message);
     });
   }
 
-  printNotification(message) {
-    this.notificationElement.innerHTML = buildNotication(message);
-
-    // ir al dom a por el boton
-    const closeButtonElement = this.notificationElement.querySelector(
-      ".notification-button-close"
+  printNotificationError(message) {
+    this.notificationElement.innerHTML = notificationErrorTemplateHTML(message);
+    const modal = this.notificationElement.querySelector(".modal");
+    modal.classList.add("show");
+    const closeModalButton = this.notificationElement.querySelector(
+      ".modal .close-button"
     );
-
+    closeModalButton.addEventListener("click", () => {
+      modal.classList.remove("show");
+    });
+  }
+  printNotificationStatus(message) {
+    this.notificationElement.innerHTML =
+      notificationStatusTemplateHTML(message);
+    // ir al dom a por el boton
+    const closeButtonElement =
+      this.notificationElement.querySelector(".btn-close");
     // asignarle un escucheador al evento click
     closeButtonElement.addEventListener("click", () => {
       this.notificationElement.innerHTML = "";
